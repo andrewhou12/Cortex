a67fd322899d653631c1121d8710f66547d3fdc1
Andrew Hou <andrewhou12@gmail.com>
2025-09-17 16:41:12 -0500

    intial dev environment set up

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,11 @@
 import express from "express";
 import sqlite3 from "sqlite3";
-import { open } from "sqlite";
 
 const app = express();
 const port = 4000;
 
 (async () => {
-  const db = await open({
-    filename: "database.sqlite",
-    driver: sqlite3.Database,
-  });
+    const db = new sqlite3.Database("database.sqlite");
 
   app.get("/api/hello", async (_, res) => {
     const rows = await db.all("SELECT datetime('now')");
--- electron/main.ts
+++ electron/main.ts
@@ -5,10 +5,11 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    webPreferences: { preload: path.join(__dirname, "preload.js") },
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
-  // Load Next.js frontend (assuming dev server on 3000)
   win.loadURL("http://localhost:3000");
 }
 
