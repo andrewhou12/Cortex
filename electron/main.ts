311802df89449e3eae8cc7516be5cc93f7db0318
Andrew Hou <andrewhou12@gmail.com>
2025-09-17 18:51:24 -0500

    translucent styling applied

--- electron/main.ts
+++ electron/main.ts
@@ -5,6 +5,12 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    frame: false, 
+    transparent: true, 
+    vibrancy: false, 
+    hasShadow: false, 
+    skipTaskbar: true, 
+    hiddenInMissionControl: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
