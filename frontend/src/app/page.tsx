5b1a898e41da24077f79c15b09f3c10634002745
Andrew Hou <andrewhou12@gmail.com>
2025-09-18 20:21:41 -0500

    latest

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="header w-full h-screen flex items-center justify-center">
       <h1 className="text-white text-3xl font-bold">
-        Full Window Card
+        Cortex Version 3
       </h1>
     </div>
   );
